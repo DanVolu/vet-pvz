76c2598fe3fe9f7803845162aedb1ac512b8b385
DanVolu <dan.voluj@gmail.com>
2025-03-13 13:49:48 +0200

    Framework changes.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from './components/navbar'
+import './App.css'
+import { Landing } from './components/sections/landing'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Landing/>
+    </>
+  )
+}
+
+export default App
