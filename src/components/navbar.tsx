a537e104c7017f79074cb0a2e0fa61a9782826a4
DanVolu <dan.voluj@gmail.com>
2025-03-14 11:03:54 +0200

    Even more landing page tweaks.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="fixed top-0 w-full bg-light-white z-50 transition-transform duration-300 bg-white">
+    <nav className="fixed top-0 w-full bg-gray-50 z-50 transition-transform duration-300 shadow-md">
       <div className="container mx-auto px-8 py-4 flex items-center justify-between">
         <div className="flex-shrink-0">
           <img
@@ -13,28 +13,28 @@ const Navbar: React.FC = () => {
         </div>
 
         <ul className="hidden xl:flex space-x-6 w-3/4 px-14">
-          <li className="text-gray-900 text-xl font-medium transition-transform transform hover:-translate-y-0.5">
+          <li className="text-gray-900 text-lg font-medium transition-transform transform hover:-translate-y-0.5">
             <a href="#klinikos">Klinikos</a>
           </li>
-          <li className="text-gray-900 text-xl font-medium transition-transform transform hover:-translate-y-0.5">
+          <li className="text-gray-900 text-lg font-medium transition-transform transform hover:-translate-y-0.5">
             <a href="#paslaugos">Paslaugos</a>
           </li>
-          <li className="text-gray-900 text-xl font-medium transition-transform transform hover:-translate-y-0.5">
+          <li className="text-gray-900 text-lg font-medium transition-transform transform hover:-translate-y-0.5">
             <a href="#kainorastis">Kainoraštis</a>
           </li>
-          <li className="text-gray-900 text-xl font-medium transition-transform transform hover:-translate-y-0.5">
+          <li className="text-gray-900 text-lg font-medium transition-transform transform hover:-translate-y-0.5">
             <a href="#atsiliepimai">Atsiliepimai</a>
           </li>
-          <li className="text-gray-900 text-xl font-medium transition-transform transform hover:-translate-y-0.5">
+          <li className="text-gray-900 text-lg font-medium transition-transform transform hover:-translate-y-0.5">
             <a href="#kontaktai">Kontaktai</a>
           </li>
         </ul>
 
-        <ul className="hidden xl:flex space-x-6 w-1/4 justify-end flex-nowrap text-white font-medium text-xl">
-          <li className="bg-gray-900 p-2 px-6 rounded-lg transition-transform transform hover:opacity-70 whitespace-nowrap">
+        <ul className="hidden xl:flex space-x-6 w-1/4 justify-end flex-nowrap text-white font-medium text-lg">
+          <li className="bg-gray-800 p-2 px-6 rounded-lg transition-transform transform hover:opacity-85 whitespace-nowrap">
             <a href="">Registracija vizitui</a>
           </li>
-          <li className="bg-gray-900 p-2 px-6 rounded-lg transition-transform transform hover:opacity-70 whitespace-nowrap">
+          <li className="bg-gray-800 p-2 px-6 rounded-lg transition-transform transform hover:opacity-85 whitespace-nowrap">
             <a href="">Iškvietimas į namus</a>
           </li>
         </ul>
--- src/components/sections/landing.tsx
+++ src/components/sections/landing.tsx
@@ -1,38 +1,46 @@
 export function Landing() {
   return (
-    <main className="flex flex-col items-center h-screen p-6 mt-">
+    <main className="flex flex-col items-center h-screen bg-gray-50 p-6">
       <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start w-full lg:w-auto h-full mt-10 lg:mt-32 mb-28 px-4 lg:px-0">
         <div className="flex flex-col items-start md:w-3/4 lg:w-2/3">
-          <h2 className="text-5xl md:text-6xl lg:text-8xl font-bold uppercase text-left lg:text-left mt-12">
+          <h2 className="text-5xl md:text-6xl lg:text-8xl font-bold uppercase text-gray-900 text-left lg:text-left mt-12 whitespace-break-spaces">
             Rūpestis, kuris gydo
           </h2>
-          <p className="my-6 text-lg lg:text-2xl font-medium text-left lg:text-left">
+          <p className="my-6 text-lg lg:text-2xl font-medium text-gray-900 opacity-85 whitespace-break-spaces">
             Greita ir profesionali pagalba Jūsų augintiniui – bet kada, kai jos
             prireikia
           </p>
-          <div className="lg:flex flex-col lg:flex-col lg:gap-4 mt-4 lg:mt-22 ">
+          <div className="lg:flex flex-col md:flex-col xl:flex-col lg:gap-4 mt-4  opacity-85">
             <div className="flex-1 p-2">
-              <h3 className="text-lg font-bold">+370 600 00000</h3>
-            </div>
-            <div className="flex-1 p-2">
-              <h3 className="text-lg font-bold">vet.paštas@gmail.com</h3>
-            </div>
-            <div className="flex-1 p-2">
-              <h3 className="text-lg font-bold">
-                Dirbame kasdien 8:00 – 18:00
-              </h3>
-            </div>
-            <div className="flex-1 p-2 mb-5">
-              <h3 className="text-lg font-bold">
+              <h3 className="text-md md:text-lg font-bold text-gray-900 whitespace-nowrap">
                 Vilnius, 01128 Vilniaus m. sav.
               </h3>
               <a
                 href="#zemelapis"
-                className="font-medium flex items-center gap-2"
+                className="font-medium flex items-center gap-2 text-gray-900 hover:text-gray-600 transition-colors"
               >
                 Rodyti žemėlapyje
-                <img src={`${import.meta.env.BASE_URL}map.png`} alt="Žemėlapio ikona" className="h-5 w-5" />
-                </a>
+                <img
+                  src={`${import.meta.env.BASE_URL}map.png`}
+                  alt="Žemėlapio ikona"
+                  className="h-5 w-5"
+                />
+              </a>
+            </div>
+            <div className="flex-1 p-2">
+              <h3 className="text-md md:text-lg font-bold text-gray-900">
+                Dirbame kasdien 8:00 – 18:00
+              </h3>
+            </div>
+            <div className="flex-1 p-2">
+              <h3 className="text-md md:text-lg font-bold text-gray-900">
+                vet.pvz.lt@gmail.com
+              </h3>
+            </div>{" "}
+            <div className="flex-1 p-2">
+              <h3 className="text-md md:text-lg font-bold text-gray-900">
+                +370 123 45678
+              </h3>
             </div>
           </div>
         </div>
@@ -41,7 +49,9 @@ export function Landing() {
           <div className="flex-1 rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
             <div
               className="relative h-[300px] md:h-[300px] bg-cover bg-center"
-              style={{ backgroundImage: `url(${import.meta.env.BASE_URL}goat.jpg)` }}
+              style={{
+                backgroundImage: `url(${import.meta.env.BASE_URL}goat.jpg)`,
+              }}
             >
               <div className="absolute inset-0 bg-black opacity-50"></div>
               <div className="relative z-10 p-6">
@@ -57,7 +67,9 @@ export function Landing() {
           <div className="flex-1 rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
             <div
               className="relative h-[300px] md:h-[300px] bg-cover bg-center"
-              style={{ backgroundImage: `url(${import.meta.env.BASE_URL}doge.jpg)` }}
+              style={{
+                backgroundImage: `url(${import.meta.env.BASE_URL}doge.jpg)`,
+              }}
             >
               <div className="absolute inset-0 bg-black opacity-50"></div>
               <div className="relative z-10 p-6">
@@ -74,7 +86,9 @@ export function Landing() {
           <div className="flex-1 rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
             <div
               className="relative h-[300px] md:h-[300px] bg-cover bg-center"
-              style={{ backgroundImage: `url(${import.meta.env.BASE_URL}vet.jpg)` }}
+              style={{
+                backgroundImage: `url(${import.meta.env.BASE_URL}vet.jpg)`,
+              }}
             >
               <div className="absolute inset-0 bg-black opacity-50"></div>
               <div className="relative z-10 p-6">
