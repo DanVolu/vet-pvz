1921616675d89e22ccb881fdb399179c0784dd8f
DanVolu <dan.voluj@gmail.com>
2025-03-14 12:14:00 +0200

    Landing page restructuring.

--- src/components/sections/landing.tsx
+++ src/components/sections/landing.tsx
@@ -1,23 +1,24 @@
 export function Landing() {
   return (
-    <main className="flex flex-col items-center h-screen bg-gray-50 p-6">
-      <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start w-full lg:w-auto h-full mt-10 lg:mt-32 mb-28 px-4 lg:px-0">
-        <div className="flex flex-col items-start md:w-3/4 lg:w-2/3">
-          <h2 className="text-5xl md:text-6xl lg:text-8xl font-bold uppercase text-gray-900 text-left lg:text-left mt-12 whitespace-break-spaces">
-            Rūpestis, kuris gydo
-          </h2>
-          <p className="my-6 text-lg lg:text-2xl font-medium text-gray-900 opacity-85 whitespace-break-spaces">
-            Greita ir profesionali pagalba Jūsų augintiniui – bet kada, kai jos
-            prireikia
-          </p>
-          <div className="lg:flex flex-col md:flex-col xl:flex-col lg:gap-4 mt-4  opacity-85">
-            <div className="flex-1 p-2">
-              <h3 className="text-md md:text-lg font-bold text-gray-900 whitespace-nowrap">
+    <main className="flex flex-col items-center min-h-screen bg-gray-50 p-6">
+      <section className="flex flex-col items-center justify-center flex-1 w-full text-center px-4">
+        <h2 className="text-5xl md:text-6xl lg:text-8xl font-bold uppercase text-gray-900 mb-6 mt-50">
+          Rūpestis, kuris gydo
+        </h2>
+        <p className="my-4 mb-10 text-lg lg:text-2xl font-medium text-gray-900 opacity-85">
+          Greita ir profesionali pagalba Jūsų augintiniui – bet kada, kai jos
+          prireikia
+        </p>
+
+        <div className="mt-12 mb-10 w-full max-w-lg">
+          <div className="flex flex-col items-center gap-6 opacity-85">
+            <div className="w-full text-center">
+              <h3 className="text-md md:text-lg font-bold text-gray-900">
                 Vilnius, 01128 Vilniaus m. sav.
               </h3>
               <a
                 href="#zemelapis"
-                className="font-medium flex items-center gap-2 text-gray-900 hover:text-gray-600 transition-colors"
+                className="font-medium flex items-center justify-center gap-2 text-gray-900 hover:text-gray-600 transition-colors"
               >
                 Rodyti žemėlapyje
                 <img
@@ -27,28 +28,30 @@ export function Landing() {
                 />
               </a>
             </div>
-            <div className="flex-1 p-2">
+            <div className="w-full text-center">
               <h3 className="text-md md:text-lg font-bold text-gray-900">
                 Dirbame kasdien 8:00 – 18:00
               </h3>
             </div>
-            <div className="flex-1 p-2">
+            <div className="w-full text-center">
               <h3 className="text-md md:text-lg font-bold text-gray-900">
                 vet.pvz.lt@gmail.com
               </h3>
-            </div>{" "}
-            <div className="flex-1 p-2">
+            </div>
+            <div className="w-full text-center">
               <h3 className="text-md md:text-lg font-bold text-gray-900">
                 +370 123 45678
               </h3>
             </div>
           </div>
         </div>
+      </section>
 
-        <div className="mt-12 lg:mt-0 w-full md:w-lg flex flex-col md:flex-col gap-6">
-          <div className="flex-1 rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
+      <div className="w-full px-4 mt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6">
+          <div className="rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
             <div
-              className="relative h-[300px] md:h-[300px] bg-cover bg-center"
+              className="relative h-[250px] bg-cover bg-center"
               style={{
                 backgroundImage: `url(${import.meta.env.BASE_URL}goat.jpg)`,
               }}
@@ -64,9 +67,9 @@ export function Landing() {
             </div>
           </div>
 
-          <div className="flex-1 rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
+          <div className="rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
             <div
-              className="relative h-[300px] md:h-[300px] bg-cover bg-center"
+              className="relative h-[250px] bg-cover bg-center"
               style={{
                 backgroundImage: `url(${import.meta.env.BASE_URL}doge.jpg)`,
               }}
@@ -83,9 +86,9 @@ export function Landing() {
             </div>
           </div>
 
-          <div className="flex-1 rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
+          <div className="rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 hover:scale-105">
             <div
-              className="relative h-[300px] md:h-[300px] bg-cover bg-center"
+              className="relative h-[250px] bg-cover bg-center"
               style={{
                 backgroundImage: `url(${import.meta.env.BASE_URL}vet.jpg)`,
               }}
